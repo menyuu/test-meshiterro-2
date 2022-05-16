2dd35a60a8a176abab5ae37847c9feb31bffd0f8
menyuu <101458830+menyuu@users.noreply.github.com>
2022-05-16 18:42:03 +0000

    画像投稿フォームの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :post_images, only: [:new, :create, :index, :show]
+
   devise_for :users
-  
+
   get 'homes/about', as: 'about'
-  
+
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

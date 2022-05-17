23c6edfe14a35827e8d59c5267b88c55ee21cadc
menyuu <101458830+menyuu@users.noreply.github.com>
2022-05-17 09:36:51 +0000

    ページネーション kaminariの導入

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,20 +1,20 @@
 class PostImage < ApplicationRecord
-  
+
   has_one_attached :image
-  
+
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   belongs_to :user
-  
+
   validates :shop_name, presence: true
   validates :image, presence: true
-  
-  
+
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -22,5 +22,5 @@ class PostImage < ApplicationRecord
     end
     image
   end
-  
+
 end
